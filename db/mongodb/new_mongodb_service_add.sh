6b401a01c99f2f9efcff16e473dd824b83c91636
Sang Hyung Pyun <im3dole@gmail.com>
2019-03-19 13:26:26 +0900

    Update new_mongodb_service_add.sh

--- db/mongodb/new_mongodb_service_add.sh
+++ db/mongodb/new_mongodb_service_add.sh
@@ -16,36 +16,34 @@ fi
 rm -rf /var/lib/${MONGO_NAME} /var/log/${MONGO_NAME} /usr/lib/systemd/system/${MONGO_NAME}.service /etc/systemd/system/multi-user.target.wants/${MONGO_NAME}.service /usr/lib/systemd/system/${MONGO_NAME}.service* /etc/${MONGO_NAME}.conf
 
 
-mkdir /var/lib/${MONGO_NAME}
-chown mongod:mongod /var/lib/${MONGO_NAME}
+mkdir                               /var/lib/${MONGO_NAME}
+chown mongod:mongod                 /var/lib/${MONGO_NAME}
 chcon -R --reference=/var/lib/mongo /var/lib/${MONGO_NAME}
 
-mkdir /var/log/${MONGO_NAME}
-chown mongod:mongod /var/log/${MONGO_NAME}
+mkdir                                 /var/log/${MONGO_NAME}
+chown mongod:mongod                   /var/log/${MONGO_NAME}
 chcon -R --reference=/var/log/mongodb /var/log/${MONGO_NAME}
 
-cp /usr/lib/systemd/system/mongod.service /usr/lib/systemd/system/${MONGO_NAME}.service
-chcon --reference=/usr/lib/systemd/system/mongod.service /usr/lib/systemd/system/${MONGO_NAME}.service
+cp /usr/lib/systemd/system/mongod.service                        /usr/lib/systemd/system/${MONGO_NAME}.service
+perl -pi.orig -e "s/MongoDB Database Server/${MONGO_DESC}/g"     /usr/lib/systemd/system/${MONGO_NAME}.service
+perl -pi      -e "s/OPTIONS=/OPTIONS=${MONGO_RUNNING_OPTION} /g" /usr/lib/systemd/system/${MONGO_NAME}.service
+perl -pi      -e "s/mongod.conf/${MONGO_NAME}.conf/g"            /usr/lib/systemd/system/${MONGO_NAME}.service
+perl -pi      -e "s/mongod.pid/${MONGO_NAME}.pid/g"              /usr/lib/systemd/system/${MONGO_NAME}.service
+chcon --reference=/usr/lib/systemd/system/mongod.service         /usr/lib/systemd/system/${MONGO_NAME}.service
 cd /etc/systemd/system/multi-user.target.wants
 ln -s /usr/lib/systemd/system/${MONGO_NAME}.service
 
-perl -pi.orig -e "s/MongoDB Database Server/${MONGO_DESC}/g" /usr/lib/systemd/system/${MONGO_NAME}.service
-perl -pi -e "s/OPTIONS=/OPTIONS=${MONGO_RUNNING_OPTION} /g" /usr/lib/systemd/system/${MONGO_NAME}.service
-perl -pi -e "s/mongod.conf/${MONGO_NAME}.conf/g" /usr/lib/systemd/system/${MONGO_NAME}.service
-perl -pi -e "s/mongod.pid/${MONGO_NAME}.pid/g" /usr/lib/systemd/system/${MONGO_NAME}.service
 
-chcon --reference=/etc/mongod.conf /etc/${MONGO_NAME}.conf
-cp  /etc/mongod.conf /etc/${MONGO_NAME}.conf
-perl -pi -e "s/27017/${MONGO_PORT}/g" /etc/${MONGO_NAME}.conf
-perl -pi -e "s/mongod.conf/${MONGO_NAME}.conf/g" /etc/${MONGO_NAME}.conf
+cp  /etc/mongod.conf                                                   /etc/${MONGO_NAME}.conf
+perl -pi -e "s/27017/${MONGO_PORT}/g"                                  /etc/${MONGO_NAME}.conf
+perl -pi -e "s/mongod.conf/${MONGO_NAME}.conf/g"                       /etc/${MONGO_NAME}.conf
 perl -pi -e "s/\/log\/mongodb\/mongod/\/log\/mongodb\/${MONGO_NAME}/g" /etc/${MONGO_NAME}.conf
 perl -pi -e "s/\/run\/mongodb\/mongod/\/run\/mongodb\/${MONGO_NAME}/g" /etc/${MONGO_NAME}.conf
-perl -pi -e "s/\/var\/lib\/mongo/\/var\/lib\/${MONGO_NAME}/g" /etc/${MONGO_NAME}.conf
-chcon --reference=/etc/mongod.conf /etc/${MONGO_NAME}.conf
-
+perl -pi -e "s/\/var\/lib\/mongo/\/var\/lib\/${MONGO_NAME}/g"          /etc/${MONGO_NAME}.conf
+chcon --reference=/etc/mongod.conf                                     /etc/${MONGO_NAME}.conf  
 
 yum install policycoreutils-python
-semanage port -a -t mongod_port_t -p tcp ${MONGO_PORT}
+semanage port --add -t mongod_port_t -p tcp ${MONGO_PORT}
 firewall-cmd --permanent --zone=public --add-port=${MONGO_PORT}/tcp
 firewall-cmd --reload
 
@@ -53,3 +51,4 @@ echo "에러 발생시 /var/log/message 파일 및 /var/log/${MONGO_NAME} 디렉
 echo "/etc/${MONGO_NAME}.conf 수정 해야함 그리고 다음 명령어 실행"
 echo "       # systemctl enable ${MONGO_NAME}"
 echo "       # systemctl start ${MONGO_NAME}"
+
