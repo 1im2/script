a51524f451b482b860e5dbfb07b2ca6353cb90d7
Sang Hyung Pyun <im3dole@gmail.com>
2019-03-19 13:07:26 +0900

    Create remove_new_mongodb_service.sh

---
+++ db/mongodb/remove_new_mongodb_service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MONGO_NAME=$1
+MONGO_PORT=$2
+
+rm -rf /var/lib/${MONGO_NAME} /var/log/${MONGO_NAME} /usr/lib/systemd/system/${MONGO_NAME}.service /etc/systemd/system/multi-user.target.wants/${MONGO_NAME}.service /usr/lib/systemd/system/${MONGO_NAME}.service* /etc/${MONGO_NAME}.conf
+
+semanage port --delete -t mongod_port_t -p tcp ${MONGO_PORT}
+firewall-cmd --permanent --zone=public --remove-port=${MONGO_PORT}/tcp
