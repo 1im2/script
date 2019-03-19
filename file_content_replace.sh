13a617b9ec0e250e41c474a605e58dc7e42a5d21
Sang Hyung Pyun <im3dole@gmail.com>
2019-03-19 10:26:19 +0900

    Create file_content_replace.sh

---
+++ file_content_replace.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "use perl: original file backup to "xxxxx.xxx".orig"
+echo ""
+perl -pi.orig -e 's/ORIG TEXT/NEW TEXT/g' /TARGET/FILE/FULL/PATH/OR/RELATIONAL/PATH
+
+echo ""
+echo ""
